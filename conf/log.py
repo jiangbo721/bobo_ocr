cf04607c4c10607e1c994ec1952501b7579bc351
liujiangbo <liujiangbo@jinlinbao.com>
2018-12-04 19:54:35 +0800

    set log and add setting but it's none

--- conf/log.py
+++ conf/log.py
@@ -17,16 +17,16 @@ mine_logger = logging.getLogger('mine')
 mine_logger.setLevel(logging.INFO)
 
 # 配置两个handler
-console_handler = logging.StreamHandler()
+# console_handler = logging.StreamHandler()
 file_handler = logging.FileHandler('./logfile/logger.log')
 
 # 配置一个formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # 给两个handler设置格式
-console_handler.setFormatter(formatter)
+# console_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 
 # 将两个handler绑定到我的logger
-mine_logger.addHandler(console_handler)
+# mine_logger.addHandler(console_handler)
 mine_logger.addHandler(file_handler)
